7a7d09361fe3c910cadf030b23aedbdb1d863411
vuongDang <vuongdang9@gmail.com>
2025-10-02 20:30:29 +0200

    bench: try to debug bench

--- examples/bench_example.rs
+++ examples/bench_example.rs
@@ -40,39 +40,41 @@ fn runtime_init() -> FooBar {
 }
 
 // The branch in this example only contains conditional on constants
-#[unsafe(no_mangle)]
-#[inline(never)]
 fn work(foo_bar: &FooBar, loop_count: u32) -> isize {
     let mut res = 0;
     // I think the testcase is too quick to have precise measurements,
     // we try to repeat the work 1000 times to smooth the imprecision
-    if foo_bar.foo && foo_bar.bar == BAR && foo_bar.b == B && foo_bar.c == C && foo_bar.d == D
-    // This condition is always true
-    {
-        // Spin loop to be able to control the amount of
-        // time spent in the branch
-        for _ in 0..loop_count {
-            // black_box to avoid loop optimizations
-            res = black_box(foo_bar.bar + res);
+    for _ in 0..1000 {
+        if foo_bar.foo && foo_bar.bar == BAR && foo_bar.b == B && foo_bar.c == C && foo_bar.d == D
+        // This condition is always true
+        {
+            // Spin loop to be able to control the amount of
+            // time spent in the branch
+            for _ in 0..loop_count {
+                // black_box to avoid loop optimizations
+                res += black_box(foo_bar.bar);
+            }
         }
     }
     res
 }
 
 // The branch in this example only contains conditional on constants
-#[unsafe(no_mangle)]
-#[inline(never)]
 fn work_with_constant(loop_count: u32) -> isize {
     const FOO_BAR: FooBar = FooBar::const_init();
     let mut res = 0;
-    if FOO_BAR.foo && FOO_BAR.bar == BAR && FOO_BAR.b == B && FOO_BAR.c == C && FOO_BAR.d == D
-    // This condition is always true
-    {
-        // Spin loop to be able to control the amount of
-        // time spent in the branch
-        for _ in 0..loop_count {
-            // black_box to avoid loop optimizations
-            res = black_box(FOO_BAR.bar + res);
+    // I think the testcase is too quick to have precise measurements,
+    // we try to repeat the work 1000 times to smooth the imprecision
+    for _ in 0..1000 {
+        if FOO_BAR.foo && FOO_BAR.bar == BAR && FOO_BAR.b == B && FOO_BAR.c == C && FOO_BAR.d == D
+        // This condition is always true
+        {
+            // Spin loop to be able to control the amount of
+            // time spent in the branch
+            for _ in 0..loop_count {
+                // black_box to avoid loop optimizations
+                res += black_box(FOO_BAR.bar);
+            }
         }
     }
     res
