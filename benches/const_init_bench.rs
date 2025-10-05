64e501e96262e9d177176027813199b595519bcd
vuongDang <vuongdang9@gmail.com>
2025-10-05 09:02:48 +0200

    sync code

--- benches/const_init_bench.rs
+++ benches/const_init_bench.rs
@@ -1,9 +1,11 @@
 //! We want to measure how much performance gain we can
 //! get by using constant initialization
 
-use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
-mod utils;
+use criterion::{
+    AxisScale, BenchmarkId, Criterion, PlotConfiguration, criterion_group, criterion_main,
+};
 use utils::*;
+mod generated_settings;
 
 // Benchmarks where initialization of data is omitted
 fn branch_optimizations(c: &mut Criterion) {
@@ -11,6 +13,10 @@ fn branch_optimizations(c: &mut Criterion) {
     // Parsing a JSON file to prevent any compiler optimization
     let foo_bar_json = FooBar::from_json_file();
     let foo_bar_constant = &FOO_BAR;
+
+    // Y scale axis
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+
     let mut group = c.benchmark_group("Branch optimizations");
     group.plot_config(plot_config);
     // let loop_counts = [1, 10, 20, 50, 100];
@@ -33,13 +39,14 @@ fn branch_optimizations_with_init_time(c: &mut Criterion) {
     let mut group = c.benchmark_group("Branch optimizations with init time");
     let loop_counts = [1, 10, 20, 50, 100];
     for loop_count in loop_counts {
+        let loop_count = LoopCountLog(10_u32.pow(loop_count));
         group.bench_with_input(
-            BenchmarkId::new("with_runtime_init_from_json", loop_count),
+            BenchmarkId::new("with_runtime_init_from_json", &loop_count),
             &loop_count,
             |b, loop_count| b.iter(|| work_init_from_json(*loop_count)),
         );
         group.bench_with_input(
-            BenchmarkId::new("with_runtime_init_from_constant", loop_count),
+            BenchmarkId::new("with_runtime_init_from_constant", &loop_count),
             &loop_count,
             |b, loop_count| b.iter(|| work_init_ref_constant(*loop_count)),
         );
@@ -51,14 +58,15 @@ fn branch_optimizations_with_init_time(c: &mut Criterion) {
         );
 
         group.bench_with_input(
-            BenchmarkId::new("with_const_init", loop_count),
+            BenchmarkId::new("with_const_init", &loop_count),
             &loop_count,
-            |b, loop_count| b.iter(|| work_constant(*loop_count)),
+            |b, loop_count| b.iter(|| work_constant(loop_count.0)),
         );
     }
     group.finish();
 }
 
+// Benchmarks where initialization of data is included
 fn branch_optimizations_with_init_time(c: &mut Criterion) {
     use with_init::*;
     // Parsing a JSON file to prevent any compiler optimization
